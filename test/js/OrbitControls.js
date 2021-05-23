5c7559ee990d909d25e24622802f56f83cddf470
Henry Egloff <henryegloff@users.noreply.github.com>
2021-05-23 10:52:15 +1000

    Update OrbitControls.js

--- test/js/OrbitControls.js
+++ test/js/OrbitControls.js
@@ -6,7 +6,7 @@ import {
 	TOUCH,
 	Vector2,
 	Vector3
-} from '../three.module.js';
+} from 'https://henryegloff.github.io/test/js/three.module.js';
 
 // This set of controls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
